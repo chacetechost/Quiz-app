6bfdb9158f4445c3aa3ed69dd26f3fc6fedb5122
Wayne Chace <realtinuijioluwa1@gmail.com>
2021-10-17 00:27:36 +0100

    quiz-app

---
+++ templates/js/questions.js
@@ -0,0 +1,223 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Who is the president of Nigeria?",
+    answer: "Muhammed Buhari",
+    options: [
+      "Alinko Dangote",
+      "Muhammed Buhari",
+      "Fatai burutai",
+      "Desmond Eliot"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which of these is a philosopher?",
+    answer: "Aristotle",
+    options: [
+      "Dj Kaywise",
+      "Bukola Saraki",
+      "Femi Faradey",
+      "Aristotle"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What day did God rest?",
+    answer: "7th day",
+    options: [
+      "Stylish Question Language",
+      "7th day",
+      "20th day",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What is a noun?",
+    answer: "A naming word",
+    options: [
+      "A naming word",
+      "A word instead of a noun",
+      "I don't know",
+      "A programming Language."
+    ]
+  },
+     {
+     numb: 6,
+     question: "what differentiates both genders?",
+     answer: "All of the above",
+     options: [
+       "Private areas",
+       "Faces",
+       "Hair styles",
+       "All of the above"
+     ]
+   },
+     {
+     numb: 7,
+     question: "Closest man to the sun?",
+     answer: "Nel armstrong",
+     options: [
+       "Nel armstrong",
+       "John Dalton",
+       "Charlse rogers",
+       "Iron man"
+     ]
+   },
+     {
+     numb: 8,
+     question: "What is 7 x 7?",
+     answer: "49",
+     options: [
+       "23",
+       "49",
+       "30",
+       "56"
+     ]
+   },
+     {
+     numb: 9,
+     question: "Who sang Hardest to love?",
+     answer: "The weekend",
+     options: [
+       "The weekend",
+       "Eminem",
+       "Don Jazzy",
+       "Scarlet Gomez"
+     ]
+   }, 
+      {
+      numb: 10,
+      question: "When did Nigeria gain Independence?",
+      answer: "1960",
+      options: [
+       "1960",
+       "1961",
+       "1962",
+       "1963"
+     ]
+   },  
+      {
+      numb: 11,
+      question: "Who wrote the national anthem?",
+      answer: "Taiwo Akinkumi",
+      options: [
+       "Taiwo Akinkumi",
+       "The chosen one",
+       "World best",
+       "I have no idea"
+     ]
+   },
+       {
+       numb: 12,
+       question: "Who is the father of Counseling?",
+       answer: "Frank Parson",
+       options: [
+        "Jack Daniels",
+        "Frank Parson",
+        "Calum Scott",
+        "John Wick"
+     ]
+   },
+       {
+       numb: 13,
+       question: "Who is the  lady with the highest I.Q?",
+       answer: "Servont",
+       options: [
+        "Servont",
+        "Jane",
+        "Casper",
+        "Tunde"
+     ]
+   },
+        {
+        numb: 14,
+        question: "What country colonized Nigeria?",
+        answer: "Britan",
+        options: [
+         "France",
+         "London",
+         "Judea",
+         "Slovakia"
+      ]
+    },
+        {
+        numb: 15,
+        question: "Who founded NYSC?",
+        answer: "Yakubu Gowon",
+        options: [
+         "Nnamdi Azikwe",
+         "Kikelomo Banjoko",
+         "Olusegun Obasanjo",
+         "Yakubu Gowon"
+      ]
+    },
+        {
+        numb: 16,
+        question: "Who invented the computer?",
+        answer: "Charlse Barbbage",
+        options: [
+         "Charlse Babbage",
+         "John Henry",
+         "Carl Rogers",
+         "Ian Lannister"
+      ]
+    },
+        {
+        numb: 17,
+        question: "What is 2 + 2?",
+        answer: "4",
+        options: [
+         "13",
+         "14",
+         "15",
+         "4"
+      ]
+    },
+        {
+        numb: 18,
+        question: "What does RSV mean in the bible?",
+        answer: "Revised Standard Version",
+        options: [
+         "Rotational Strong view",
+         "Revised Standard Version",
+         "R S V",
+         "Remember Saint Veron"
+      ]
+    },
+        {
+        numb: 19,
+        question: "Who created heaven and earth",
+        answer: "Almighty God",
+        options: [
+         "Almight God",
+         "Almighty God",
+         "God",
+         "God Almighty"
+      ]
+    },
+        {
+        numb: 20,
+        question: "Who sang Only You?",
+        answer: "Calum Scott",
+        options: [
+         "Dj Khaled",
+         "Calum Scott",
+         "Beyonce",
+         "Micheal Jackson"
+      ]
+    },
+];
